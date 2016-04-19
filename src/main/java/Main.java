7a3bb2e9969c12470f5b3abebec9e162909542ef
Евгений <deyneko55@gmail.com>
2016-04-19 21:30:26 +0300

    add first class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
 
 public class Main {
     public static void main(String[] args) {
-        int i = 10;
-        System.out.println(i++);
-        System.out.println(++i);
+
+        //int  u, g = 10, h;
+
+        int u;
+        int g = 10;
+        int h = 0;
+
+        byte b = (byte)345;
+
+        float f = (float)20.5;
+
+        boolean b1 = false;
+        boolean b2 = false;
+
+        char c1 = 'g';
+        char c2 = 'h';
+        char c3 = 'H';
+
+       /* System.out.println((int)c1);
+        System.out.println((int)c2);
+        System.out.println((int)c3);*/
+
+        System.out.println(11 % 3);
+
+        if (g == 13) {
+            System.out.println(g);
+        }
+
     }
 }
