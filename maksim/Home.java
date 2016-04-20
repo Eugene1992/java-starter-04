0891257393b6e04dae8fb5c4a2c6cae9de823d2f
FunnyDot <xmaksya@gmail.com>
2016-04-20 03:18:45 +0300

    Added files via upload

---
+++ maksim/Home.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Max on 20.04.2016.
+ */
+public class Home {
+    public static void main(String[] args) {
+      /*  byte a = 2;
+        byte b = 4;
+        byte c = a + b;
+        System.out.print(c);*/
+      /*  short a = 6;
+        short b = 5;
+        short c = (short) (a - b);
+        System.out.println(c);*/
+
+        /*int a =10;
+        int b = 5;
+        int c = a / b;
+        System.out.println(c);*/
+        /*    long a = 2;
+        long b = 54;
+        long c = a * b;
+        System.out.println(c);*/
+
+       /* short a = 26;
+        short b = 24;
+        byte c = 7;
+        byte d = (byte) ((byte) (a + b )/( c));
+        System.out.println(d);*/
+//task
+        int a = 11;
+        int b = a / 2;
+        int c = b / 2;
+        int d = c / 2 ;
+        System.out.println(d);
+
+
+    }
+}
