0d29802393757390d0055cc679870127c2abe99b
IreneD16 <ira.deordieva@mail.ru>
2016-04-20 12:31:38 +0300

    hw02

---
+++ ira/main/hw02.java
@@ -0,0 +1,34 @@
+/**
+ * Created by notebook on 20.04.2016.
+ */
+public class hw02 {
+    public static void main(String[] args) {
+        byte a;
+        short b;
+        int c;
+        long d;
+
+        a = 2+3;
+        b = 18-6;
+        c = 36/6;
+        d = 368*5;
+
+        byte a1 = 7, a2 = 6;
+        byte a3 = (byte) (a1 + a2);
+
+        short b1 = 24;
+        byte a4 = (byte) ((b+b1)/a3);
+
+        short b2 = (short) (b1/b);
+        byte a5 = 10/5;
+        int c1 = b2*a5;
+
+        c1 = c1 + c;
+        b2 = (short) (b2 * b);
+
+        a = (byte) (a + a1);
+        a1 = (byte) (a1 * a);
+
+    }
+}
+
---
+++ ira/main/hw02Bonus.java
@@ -0,0 +1,32 @@
+import static java.lang.System.*;
+
+/**
+ * Created by notebook on 20.04.2016.
+ */
+public class hw02Bonus {
+    public static void main(String[] args) {
+        byte a = 53;
+
+        byte a1 = (byte) (a%2);
+        byte a2 = (byte) (26%2);
+        byte a3 = (byte) (13%2);
+        byte a4 = (byte) (6%2);
+        byte a5 = (byte) (3%2);
+        byte a6 = (byte) 1;
+        byte a7 = (byte) 0;
+        byte a8 = (byte) 0;
+
+        System.out.print(a8);
+        System.out.print(a7);
+        System.out.print(a6);
+        System.out.print(a5);
+        System.out.print(a4);
+        System.out.print(a3);
+        System.out.print(a2);
+        System.out.print(a1);
+
+
+
+
+    }
+}
