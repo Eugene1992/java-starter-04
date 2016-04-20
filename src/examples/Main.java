b5a08b6c4b85837819a9f8abb9dbbfdb1beec3c9
Eugene <deyneko55@gmail.com>
2016-04-20 18:11:13 +0300

    Add test

--- src/examples/Main.java
+++ src/examples/Main.java
@@ -1,33 +1,9 @@
+import java.io.IOException;
+
+import static other.test.Test.*;
 
 public class Main {
-    public static void main(String[] args) {
-
-        //int  u, g = 10, h;
-
-        int u;
-        int g = 10;
-        int h = 0;
-
-        byte b = (byte)345;
-
-        float f = (float)20.5;
-
-        boolean b1 = false;
-        boolean b2 = false;
-
-        char c1 = 'g';
-        char c2 = 'h';
-        char c3 = 'H';
-
-       /* System.out.println((int)c1);
-        System.out.println((int)c2);
-        System.out.println((int)c3);*/
-
-        System.out.println(11 % 3);
-
-        if (g == 13) {
-            System.out.println(g);
-        }
-
+    public static void main(String[] args) throws IOException, InterruptedException {
+        runTest();
     }
 }
