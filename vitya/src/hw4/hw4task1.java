9357e8643d51b3cb3a7329c726b35ea9d4002e8a
Ethersmike <vganich@hotmail.com>
2016-04-22 13:41:52 +0300

    hw3(not finished)

---
+++ vitya/src/hw4/hw4task1.java
@@ -0,0 +1,14 @@
+package hw4;
+
+/**
+ * Created by User on 22.04.2016.
+ */
+public class hw4task1 {
+    public static void main(String[] args) {
+
+       for (int a =1000;a<=9999;a=a+3){
+           System.out.println(a+" ");
+       }
+
+}
+}
