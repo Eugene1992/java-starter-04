490233c8eb472150d26022e6a88746c5fb265263
Ethersmike <vganich@hotmail.com>
2016-04-26 08:41:08 +0300

    Classwork

--- vitya/src/classwork/Classwork.java
+++ vitya/src/classwork/Classwork.java
@@ -1,10 +1,49 @@
 package classwork;
 
+import java.util.Scanner;
+
 /**
  * Created by ПК on 25.04.2016.
  */
 public class Classwork {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число деления : ");
+        int a = scan.nextInt();
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Введите число деления : ");
+        int b = scan2.nextInt();
+    }
+
+public static int getHightNumber(int a,int b) {
+    int max=1;
+    if (a<b) {
+        for (int i = 1;  i<=a ;i++ ) {
+        if ()
+
+        }
+
+}
+
+
+
+//Найти найбольшой делитель...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
