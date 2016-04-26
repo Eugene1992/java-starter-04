cbc3e3d30d3da7ea26e2deb7fd1731fc23a47fe6
Андрей <akon_andrey1989@mail.ru>
2016-04-26 15:00:45 +0300

    Reformed

--- andrey/homew/hw4/Chess.java
+++ andrey/homew/hw4/Chess.java
@@ -1,29 +1,34 @@
 package hw4;
-//Не дороблене!
 /*
  В этом блоке задач используйте минимальный вывод в строку, то есть "Ж " или "- ", а не целыми строками
 "Ж Ж Ж Ж - - - - Ж Ж Ж Ж - - - - Ж Ж Ж Ж - - - - Ж Ж Ж Ж - - - -". Комбинируйте вложенными циклами и условиями if или switch.
 */
 import java.util.Scanner;
-
 public class Chess {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите размерность доски: ");
+        System.out.println(" Введите размерность доски: ");
         int n = sc.nextInt();
         int m = sc.nextInt();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++)
-                if((i+j)%2==0)
-                    System.out.print("----");
-                else
-                    System.out.print("ЖЖЖЖ ");
+        for(int a = 0; a <= n ; a++){
+            for(int b = 0; b <=m ; b++) {
+                if (a % 8 <= 3) {
+                    if (b % 8 <= 3) {
+                        System.out.print(" Ж ");
+                    } else {
+                        System.out.print(" - ");
+                    }
+                } else {
+                    if (b % 8 <= 3) {
+                        System.out.print(" - ");
+                    } else {
+                        System.out.print(" Ж ");
+                    }
+                }
+            }
             System.out.println();
         }
-
     }
-
 }
 
 
